3e6b5babcfcdf649d4b27919540baa52821e696d
learnersubha <learnersubha0@gmail.com>
2025-02-28 20:12:45 +0530

    Update failure.groovy

--- vars/failure.groovy
+++ vars/failure.groovy
@@ -1,5 +1,5 @@
 def call(String sender,String receiver){
-     emailext from: '${sender}' 
+     emailext from: '${sender}', 
         to:'${receiver}',
         body: 'Bad news: your build was failed',
         subject: 'build failure'
