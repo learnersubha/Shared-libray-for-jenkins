e69d04d22d7c6f8dacc278cacee5b51be00cd2b9
learnersubha <learnersubha0@gmail.com>
2025-02-28 16:53:42 +0530

    Create success.groovy

---
+++ vars/success.groovy
@@ -0,0 +1,6 @@
+def call(String "sender", String "receiver"){
+   emailext from:'sender',
+        to:'receiver',
+        body: 'Godde news: your build was successful',
+        subject: 'build successful'
+}
