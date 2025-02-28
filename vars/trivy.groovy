1a6117ad093ead96d7198f7c4b3a9186b5fa8924
learnersubha <learnersubha0@gmail.com>
2025-02-28 21:19:46 +0530

    Update trivy.groovy

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -1,3 +1,9 @@
 def call(){  
-  sh "trivy fs . -o result.json"
+  sh "trivy fs . -o result.json",
+     emailext (
+            subject: "Trivy Scan Results",
+            body: "Please find attached the Trivy scan results.",
+            attachmentsPattern: "result.json",
+            to: "learnersubha0@gmail.com" 
+       )
 }
