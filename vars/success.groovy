3b8aa09b14a32b2cd48496b35c2b08a4dfc142b3
learnersubha <learnersubha0@gmail.com>
2025-02-28 17:09:55 +0530

    Update success.groovy

--- vars/success.groovy
+++ vars/success.groovy
@@ -1,6 +1,6 @@
 def call(String sender, String receiver){
-   emailext from:'sender',
-        to:'receiver',
+   emailext from:'${sender}',
+        to:'${receiver}',
         body: 'Good news: your build was successful',
         subject: 'build successful'
 }
